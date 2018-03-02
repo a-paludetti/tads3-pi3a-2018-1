ba3150c44c8b734ea56aa59098b1fba33efa8d2a
a-paludetti <a.paludetti@gmail.com>
2018-03-02 13:42:31 -0300

    incluido classes de consulta e alteração de produto, Consulta de produto como texto apenas.

---
+++ src/main/java/com/senac/tads/AlterarProduto.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.senac.tads;
+import com.senac.tads.Produto;
+import com.senac.tads.ProdutoDAO;
+
+/**
+ *
+ * @author Red
+ */
+public class AlterarProduto {
+    
+}
